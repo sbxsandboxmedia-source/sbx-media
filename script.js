0a2674ae00af33a788f508b5e4321b4181be69ca
sbxsandboxmedia-source <sbx.sandboxmedia@gmail.com>
2026-06-19 13:29:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,38 @@ if(menuBtn){
     navLinks.classList.toggle("show");
   });
 }
+const counters = document.querySelectorAll(".counter");
+
+const runCounter = () => {
+  counters.forEach(counter => {
+    const target = +counter.getAttribute("data-target");
+    let count = 0;
+
+    const updateCounter = () => {
+      const increment = target / 50;
+
+      if (count < target) {
+        count += increment;
+        counter.innerText = Math.ceil(count);
+        setTimeout(updateCounter, 30);
+      } else {
+        counter.innerText = target + "+";
+      }
+    };
+
+    updateCounter();
+  });
+};
+
+const statsSection = document.querySelector(".stats-section");
+
+if (statsSection) {
+  const observer = new IntersectionObserver(entries => {
+    if (entries[0].isIntersecting) {
+      runCounter();
+      observer.disconnect();
+    }
+  });
+
+  observer.observe(statsSection);
+}
